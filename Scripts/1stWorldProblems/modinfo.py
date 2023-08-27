3d5a0d6d0470fd2352a07d53bc3b9e40585f93f6
Richard Hein <richard.hein@cognilore.com>
2023-08-27 13:43:29 -0400

    rename sample mod to 1stWorldProblems

--- Scripts/1stWorldProblems/modinfo.py
+++ Scripts/1stWorldProblems/modinfo.py
@@ -2,24 +2,25 @@ from sims4communitylib.mod_support.common_mod_info import CommonModInfo
 
 
 class ModInfo(CommonModInfo):
-    """ Mod info for the S4CL Sample Mod. """
+    """Mod info for the 1st World Problems Mod."""
+
     # To create a Mod Identity for this mod, simply do ModInfo.get_identity(). Please refrain from using the ModInfo of The Sims 4 Community Library in your own mod and instead use yours!
     _FILE_PATH: str = str(__file__)
 
     @property
     def _name(self) -> str:
         # This is the name that'll be used whenever a Messages.txt or Exceptions.txt file is created <_name>_Messages.txt and <_name>_Exceptions.txt.
-        return 'S4CLSampleMod'
+        return "1stWorldProblems"
 
     @property
     def _author(self) -> str:
         # This is your name.
-        return 'ColonolNutty'
+        return "Auxon"
 
     @property
     def _base_namespace(self) -> str:
         # This is the name of the root package
-        return 's4cl_sample_mod_scripts'
+        return "1stWorldProblems"
 
     @property
     def _file_path(self) -> str:
