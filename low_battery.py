4072cde07eeac9748fb07f8728196176005ffc19
Richard Hein <richard.hein@cognilore.com>
2023-08-27 12:30:22 -0400

    aider: Added low_battery.py mod_behaviors.py mod_main.py mod_utils.py

---
+++ low_battery.py
@@ -0,0 +1,13 @@
+from sims4communitylib.events.event_handling.common_event_registry import CommonEventRegistry
+from sims4communitylib.events.zone_spin.update_tuning_event import UpdateTuningEvent
+from sims4communitylib.events.zone_spin.zone_update_event import ZoneUpdateEvent
+from sims4communitylib.modinfo import ModInfo
+from sims4communitylib.utils.resources.common_capability_loot_actions import CommonCapabilityLootActions
+from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
+from sims4communitylib.utils.sims.common_sim_name_utils import CommonSimNameUtils
+
+def on_low_battery_reaction(event_data: UpdateTuningEvent):
+    sim_info = CommonSimUtils.get_sim_info(event_data.sim_id)
+    if sim_info is not None:
+        # Replace with appropriate reaction logic (e.g., change mood, play animation)
+        sim_info.show_notification('Oh no! My phone is about to die!')
--- mod_main.py
+++ mod_main.py
@@ -1,6 +1,7 @@
 import mod_behaviors
 import mod_utils
 
+
 def main():
     # This function will be called by the game.
     # Add code here to initialize your mod.
